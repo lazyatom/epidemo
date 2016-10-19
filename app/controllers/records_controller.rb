0c62e9f6136eccdbf149ffc4147cacc9b3f22469
James Adam <james@lazyatom.com>
2016-10-19 09:47:51 +0100

    Add new simpler URL for scanning

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,11 +2,11 @@ class RecordsController < ApplicationController
   before_filter :check_secret
 
   def new
-    @record = Record.new(record_params)
+    @record = Record.new(record_attributes)
   end
 
   def create
-    @record = Record.new(record_params)
+    @record = Record.new(record_attributes)
     if @record.save
       redirect_to edit_record_path(@record, secret: Rails.application.secrets.http_password)
     else
@@ -20,7 +20,7 @@ class RecordsController < ApplicationController
 
   def update
     @record = Record.find(params[:id])
-    if @record.update_attributes(record_params)
+    if @record.update_attributes(record_attributes)
       flash.now[:notice] = "Record updated"
     else
       flash.now[:alert] = "There was a problem updating this record"
@@ -34,8 +34,14 @@ class RecordsController < ApplicationController
     raise "NO" unless params[:secret] == Rails.application.secrets.http_password
   end
 
-  def record_params
-    params.require(:record).permit(:source_id, :gender, :age_range, :infection_location, :scanned_by)
+  def record_attributes
+    if params[:record]
+      params.require(:record).permit(:source_id, :gender, :age_range, :infection_location, :scanned_by)
+    elsif params[:scanned_by]
+      params.permit(:source_id, :scanned_by)
+    else
+      raise "Malformed params: #{params}"
+    end
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :records, only: [:new, :create, :edit, :update]
 
   get "/create", to: "records#new"
+  get "/record/:secret/:source_id(/:scanned_by)", to: "records#new"
 
   get "/qr_code", to: "qr_codes#show"
   get "/data", to: "data#index"
